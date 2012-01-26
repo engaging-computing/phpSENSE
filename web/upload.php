939a1ef958812c0d40d78e435b9ff3b04f2bdd23
Chris Adoretti <adorettic@gmail.com>
2012-01-26 01:02:53 -0500

    Resolving issue #26 upload.php fix

--- web/upload.php
+++ web/upload.php
@@ -721,10 +721,8 @@ if(isset($_POST['session_create']) && count($errors) == 0) {
                 } 
                 
                 else {
-					//TODO: Discover the true nature of this commented out echo
-                    // This value is no a time, so just plug in to the row record
-			        //echo $x[] = $val;
-					;
+                    // This value is not a time type, so we directly insert into the row
+			        $x[] = $val;
 			    }
 		    }
 		    
