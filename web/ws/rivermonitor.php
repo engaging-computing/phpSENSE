617445ddf5528f624e0f3769ca54aa44647fef63
Chris Adoretti <adorettic@gmail.com>
2012-04-26 12:15:30 -0400

    RIVER MONSTA

--- web/ws/rivermonitor.php
+++ web/ws/rivermonitor.php
@@ -123,7 +123,7 @@
         echo $writeStr;
 
 
-        $data = array(array($_SERVER['REQUEST_TIME'],42.64037, -71.352156, $temperature, $exTemperature, $pH, $flowRate));
+        $data = array(array($_SERVER['REQUEST_TIME'] * 1000,42.64037, -71.352156, $temperature, $exTemperature, $pH, $flowRate));
 
         $response = putData(408,3341, $data);
 	
