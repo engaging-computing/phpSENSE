92a11567b6f809e057a59ca8243e321b788bf80d
jdalphon <jdalphon@jdalphon-isense.(none)>
2012-05-30 13:35:49 -0400

    Added functionality to get all conversions associated with a unit_id

---
+++ web/includes/api/conversions.php
@@ -0,0 +1,20 @@
+<?php
+
+    /* Get all conversions associated with a given unit_id*/
+    function getPossibleConversions($unit_id){
+        global $db;
+
+	$sql = "SELECT conversions.name,
+		  conversions.from_unit_id,
+		  conversions.to_unit_id,
+		  conversions.javascript_code,
+		  units.abbreviation 
+		FROM conversions,units 
+		WHERE conversions.from_unit_id={$unit_id} 
+		AND units.unit_id=conversions.to_unit_id";
+        $query = $db->query($sql);
+
+        return $query;
+    }
+
+?>
