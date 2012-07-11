b1149d31d9611a9ce2503ff255c4a8e528fb0acb
Hassan McKusick <Hassan.McKusick@gmail.com>
2012-07-11 15:55:53 -0400

    added Admin lock and verification to timeFix.php

--- web/ws/timeFix.php
+++ web/ws/timeFix.php
@@ -30,63 +30,135 @@ require_once '../includes/config.php';
 
 $tmpusr = $session->getUser();
 
-if($tmpusr['administrator']) {
+global $mdb;
 
-    if(isset($_REQUEST['eids'])) {
-        global $mdb;
-    
-        $eids = explode(" ", $_REQUEST['eids']);
-    
-        if ($eids[0] == "all") {
-            $eids = array();
-            echo "<pre>";
-            foreach ($mdb->db->listCollections() as $index=>$coll) {
-                $name = $coll->getName();
+
+if ($tmpusr['administrator']) {
+    if (isset($_REQUEST['eids'])) {        
+        
+        if (isset($_REQUEST['verify']) && $_REQUEST['verify'] == 'yes') {
             
-                if ($name[0] == 'e') {
-                    $eids[] = trim($name, 'e');
+            $eids = explode(" ", $_REQUEST['eids']);
+
+            if ($eids[0] == "all") {
+                $eids = array();
+                echo "<pre>";
+                foreach ($mdb->db->listCollections() as $index => $coll) {
+                    $name = $coll->getName();
+
+                    if ($name[0] == 'e') {
+                        $eids[] = trim($name, 'e');
+                    }
                 }
+                echo "</pre>";
             }
-            echo "</pre>";
-        }
-    
-        echo "Starting on... <pre>" . print_r($eids, true) . "</pre><br>";
-    
-        foreach ($eids as $index=>$eid) {
-            $fields = getFields($eid);
-        
-            echo 'Trying EID:' . $eid . '<br>';
-        
-            foreach ($fields as $fieldIndex=>$field) {
-                if ($field['type_id'] == 7) {
-                    //Do Time Fix
+
+            echo "Starting on... <pre>" . print_r($eids, true) . "</pre><br>";
+            
+            foreach ($eids as $index => $eid) {
+                $fields = getFields($eid);
                 
-                    $func =  "function fix() {
-                        var ret = [];
-                        var data = db.e" . $eid . ".find({}).forEach(function(obj) {
-                            var fixed = new Date(obj[\"" . $field['field_name'] . "\"] + ' GMT');
-                            var replacement;
+                echo 'Trying EID:' . $eid . '<br>';
+                
+                foreach ($fields as $fieldIndex => $field) {
+                    if ($field['type_id'] == 7) {
+                        //Do Time Fix
                         
-                            if (!isNaN(fixed.valueOf())) {
-                                replacement = NumberLong(fixed.valueOf());
-                            }
-                            else if (!isNaN(Number(obj[\"" . $field['field_name'] . "\"]))){
-                                replacement = NumberLong(Number(obj[\"" . $field['field_name'] . "\"]));
-                            }
-                            else {
-                                replacement = 'NaN';
-                            }
+                        $func = "function fix() {
+                            var ret = [];
+                            var data = db.e" . $eid . ".find({}).forEach(function(obj) {
+                                var fixed = new Date(obj[\"" . $field['field_name'] . "\"] + ' GMT');
+                                var replacement;
+                        
+                                if (!isNaN(fixed.valueOf())) {
+                                    replacement = NumberLong(fixed.valueOf());
+                                }
+                                else if (!isNaN(Number(obj[\"" . $field['field_name'] . "\"]))){
+                                    replacement = NumberLong(Number(obj[\"" . $field['field_name'] . "\"]));
+                                }
+                                else {
+                                    replacement = 'NaN';
+                                }
                         
                         
-                            ret.push('Replacing ' + obj[\"" . $field['field_name'] . "\"] + ' with ' + replacement + '\\r\\n');
-                            }); return ret;}";
-                        //db.e" . $eid . ".update({_id : obj['_id']}, {\$set : {" . $field['field_name'] . " : replacement}});
+                                ret.push('Replacing ' + obj[\"" . $field['field_name'] . "\"] + ' with ' + replacement + '\\r\\n');
+                                
+                                db.e" . $eid . ".update({_id : obj['_id']}, {\$set : {" . $field['field_name'] . " : replacement}});
+                                                                
+                                });
+                                 return ret;}";
                         echo "<pre>";
-                        print_r( $mdb->db->execute($func));
+                        print_r($mdb->db->execute($func));
                         echo "</pre>";
                     }
                 }
             }
+        } else {
+            $eids = explode(" ", $_REQUEST['eids']);
+
+            if ($eids[0] == "all") {
+                $eids = array();
+                echo "<pre>";
+                foreach ($mdb->db->listCollections() as $index => $coll) {
+                    $name = $coll->getName();
+
+                    if ($name[0] == 'e') {
+                        $eids[] = trim($name, 'e');
+                    }
+                }
+                echo "</pre>";
+            }
+
+            echo "<a href='timeFix.php?eids=";
+            foreach($eids as $index => $e) {
+                if( $index != sizeof($eids)-1)
+                    echo $e . "+";
+                else
+                    echo $e;
+            }
+            echo "&verify=yes'>Click here to Verify!</a><br>";
+            
+            foreach ($eids as $index => $eid) {
+                $fields = getFields($eid);
+                
+                //echo 'Trying EID:' . $eid . '<br>';
+                
+                foreach ($fields as $fieldIndex => $field) {
+                    if ($field['type_id'] == 7) {
+                        //Do Time Fix
+                        
+                        $func = "function fix() {
+                            var ret = [];
+                            var data = db.e" . $eid . ".find({}).forEach(function(obj) {
+                                var fixed = new Date(obj[\"" . $field['field_name'] . "\"] + ' GMT');
+                                var replacement;
+                        
+                                if (!isNaN(fixed.valueOf())) {
+                                    replacement = NumberLong(fixed.valueOf());
+                                }
+                                else if (!isNaN(Number(obj[\"" . $field['field_name'] . "\"]))){
+                                    replacement = NumberLong(Number(obj[\"" . $field['field_name'] . "\"]));
+                                }
+                                else {
+                                    replacement = 'NaN';
+                                }
+                        
+                        
+                                ret.push('Replacing ' + obj[\"" . $field['field_name'] . "\"] + ' with ' + replacement + '\\r\\n');                                                                
+                                });
+                                 return ret;}";
+                                 
+                        $returnVal = $mdb->db->execute($func);
+                        
+                        echo "<pre><table>";
+                        foreach($returnVal['retval'] as $val) {
+                            echo "<tr><td>" . $val . "</td></tr>";
+                        }
+                        echo "</table></pre>";
+                    }
+                }
+            }
         }
-    }
+    } 
+}
 ?>
