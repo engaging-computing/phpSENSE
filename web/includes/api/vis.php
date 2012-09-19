389449938c507268de6fb95e6589e667749b670f
Michael McGuinness <mmcguinn@cs.uml.edu>
2012-09-19 14:06:57 -0400

    -Fixed saved vis api (since wierd git error).

--- web/includes/api/vis.php
+++ web/includes/api/vis.php
@@ -26,80 +26,6 @@
  * DAMAGE.
  */
 
-function createNewVis($uid, $eid, $name, $desc, $sessions, $url_params, $is_activity = 0, $hidden = 0) {
-	global $db;
-	
-	$db->query("INSERT INTO visualizations (`owner_id`, `experiment_id`, `name`, `description`, `url_params`, `is_activity`, `hidden`, `timecreated`, `timemodified`) VALUES({$uid}, {$eid}, '{$name}', '{$desc}', '{$url_params}', {$is_activity}, {$hidden}, NOW(), NOW())");
-	
-	if($db->numOfRows) {
-		$vid = $db->lastInsertId();
-		
-		foreach($sessions as $session) {
-			addSessionToVis($vid, $session);
-		}
-		
-		return $vid;
-	}
-	
-	return false;
-}
-
-function addSessionToVis($vid, $sid) {
-	global $db;
-	
-	$output = $db->query("INSERT INTO visualizationSessionMap (`vis_id`, `session_id`) VALUES({$vid}, {$sid})");
-	
-	if($db->numOfRows) {
-	    updateTimeModifiedForVis($vid);
-		return true;
-	}
-	
-	return false;
-}
-
-function getVisByUser($uid) {
-	global $db;
-	
-	$output = $db->query("SELECT 	visualizations.vis_id, 
-									visualizations.owner_id, 
-									visualizations.experiment_id, 
-									visualizations.name, 
-									visualizations.url_params,
-									visualizations.hidden, 
-									visualizations.timecreated,
-									visualizations.description,
-									visualizations.timecreated AS `timeobj`,
-									experiments.name AS `experiment_name`, 
-									experiments.description  AS `experiment_description`,
-									experiments.hidden AS `experiment_hidden`
-									FROM visualizations, experiments 
-									WHERE visualizations.experiment_id = experiments.experiment_id
-									AND visualizations.owner_id = {$uid}
-									AND visualizations.is_activity = 0");
-	
-	if($db->numOfRows) {
-		for($i = 0; $i < count($output); $i++) {
-			$output[$i]['sessions'] = implode("+",getSessionsForVis($output[$i]['vis_id']));
-		}
-		
-		return $output;
-	}
-	
-	return false;
-}
-
-function getVisByExperiment($eid) {
-	global $db;
-	
-	$output = $db->query("SELECT * FROM visualizations WHERE experiment_id = {$eid} AND is_activity = 0");
-	
-	if($db->numOfRows) {
-		return $output;
-	}
-	
-	return false;
-}
-
 function getVisByTag($tag) {
 	global $db;
 	
@@ -115,7 +41,7 @@ function getVisByTag($tag) {
 		$results = array();
 		
 		foreach($output as $out) {
-			$vises = getVisByExperiment($out['experiment_id']);
+			$vises = getSavedVisByExperiment($out['experiment_id']);
 
 			if($vises) {
 				foreach($vises as $v) {
@@ -133,11 +59,11 @@ function getVisByTag($tag) {
 function hideVis($vid) {
 	global $db;
 	
-	$db->query("UPDATE visualizations SET hidden = 1 WHERE vis_id = {$vid}");
+	$db->query("UPDATE savedVises SET hidden = 1 WHERE vid = {$vid}");
 	
 	if($db->numOfRows) {
 	    updateTimeModifiedForVis($vid);
-		return $db->lastInsertId();
+		return true;
 	}
 	
 	return false;
@@ -146,124 +72,24 @@ function hideVis($vid) {
 function showVis($vid) {
 	global $db;
 	
-	$db->query("UPDATE visualizations SET hidden = 0 WHERE vis_id = {$vid}");
+	$db->query("UPDATE savedVises SET hidden = 0 WHERE vid = {$vid}");
 	
 	if($db->numOfRows) {
-		return $db->lastInsertId();
+		return true;
 	}
 	
 	return false;
 }
 
-function getSessionsForVis($vid) {
-	global $db;
-	
-	$output = $db->query("SELECT session_id FROM visualizationSessionMap WHERE vis_id = {$vid}");
-	
-	$data = array();
-	if($db->numOfRows) {
-		foreach($output as $o) {
-			array_push($data, $o['session_id']);
-		}
-		
-		return $data;
-	}
-	
-	return false;
-}
-
-function getVisById($vid) {
-	global $db;
-	
-	$output = $db->query("SELECT * FROM visualizations WHERE vis_id = {$vid} LIMIT 0, 1");
-	
-	if($db->numOfRows) {
-		
-		$compress = array();
-		$sessions = getSessionsForVis($vid);
-		/*
-		foreach($sessions as $sid) {
-			$compress[] = $sid['session_id'];
-		}
-		
-		$compress = implode(" ", $compress);
-		*/
-		$output[0]['sessions'] = implode(" ", $sessions);
-		
-		return $output[0];
-	}
-	
-	return false;
-}
-
-function getNumberOfVisualizations() {
-	global $db;
-	
-	$output = $db->query("SELECT COUNT(*) AS `count` FROM visualizations WHERE visualizations.is_activity = 0");
-	
-	return $output[0]['count'];
-}
-
-function updateTimeModifiedForVis($vid) {
-    global $db;
-    
-    $sql = "UPDATE `visualizations` SET visualizations .timemodified = NOW() WHERE visualizations.vis_id = {$vid}";
-    $db->query($sql);
-    
-    return true;
-}
-
-function addFeaturedVis($vid) {
-	global $db;
-	
-	$db->query("UPDATE visualizations SET featured = 1 WHERE vis_id = {$vid}");
-	
-	$output = $db->query("SELECT name from visualizations WHERE vis_id = {$vid}");
-	
-	$name = "";
-	if($db->numOfRows) {
-		$name = $output[0]['name'];
-	}
-	/*
-	if($name != "") {
-		publishToTwitter('Latest Featured Visualization: "'.$name.'" - http://isense.cs.uml.edu/rc1/vis.php?id='.$vid);
-	}
-	*/
-	updateTimeModifiedForVis($vid);
-	
-	return true;
-}
-
-function removeFeaturedVis($vid) {
-	global $db;
-	
-	$db->query("UPDATE visualizations SET featured = 0 WHERE vis_id = {$vid}");
-	
-	updateTimeModifiedForVis($vid);
-	
-	return true;
-}
-
-function setVisualizationPictureId( $vid, $purl ){
-    global $db;
-    
-    $db->query("UPDATE visualizations SET picture_url = \"{$purl}\" WHERE vis_id = {$vid}");
-    
-    return true;
-    
-}
-
-
-/**Functions for saved vises for HIGHVIS **/
-function storeSavedVis($owner,$experiment,$title,$description,$json){
+function storeSavedVis($owner,$experiment,$title,$description,$data,$globals){
     global $db;
 
-    $sql = "INSERT INTO savedVises (owner_id,experiment_id,title,description,json) VALUES ({$owner},{$experiment},\"{$title}\",\"{$description}\",\"{$json}\")";
+    $sql = "INSERT INTO savedVises (owner_id,experiment_id,title,description,data,globals) VALUES ({$owner},{$experiment},\"{$title}\",\"{$description}\",\"{$data}\",\"{$globals}\")";
 
     $db->query($sql);
 
     if($db->numOfRows){
-        return true;
+        return $db->lastInsertId();
     }
 
     return false;
@@ -282,7 +108,8 @@ function getSavedVis($vid){
 function getSavedVisDesc($vid){
     global $db;
 
-    $sql = "SELECT title, description FROM savedVises where vid = {$vid}";
+    $sql = "SELECT vid, title, description, hidden, owner_id, timecreated, as `timeobj`
+            FROM savedVises where vid = {$vid}";
 
     $output = $db->query($sql);
 
@@ -296,8 +123,7 @@ function getSavedVisDesc($vid){
 function getSavedVisByExperiment($experiment_id){
     global $db;
 
-    $sql = "SELECT *,
-            timecreated as `timeobj`
+    $sql = "SELECT vid, title, description, hidden, owner_id, timecreated, as `timeobj`
             FROM savedVises where experiment_id = {$experiment_id}";
 
     $output = $db->query($sql);
@@ -308,8 +134,7 @@ function getSavedVisByExperiment($experiment_id){
 function getSavedVisByOwner($owner_id){
     global $db;
 
-    $sql = "SELECT *,
-            timecreated as `timeobj`
+    $sql = "SELECT vid, title, description, hidden, owner_id, timecreated, as `timeobj`
             FROM savedVises where owner_id = {$owner_id}";
     
     $output = $db->query($sql);
@@ -320,8 +145,7 @@ function getSavedVisByOwner($owner_id){
 function getAllSavedVises(){
     global $db;
 
-    $sql = "SELECT *,
-            timecreated as `timeobj`
+    $sql = "SELECT vid, title, description, hidden, owner_id, timecreated, as `timeobj`
             FROM savedVises";
     
     $output = $db->query($sql);
